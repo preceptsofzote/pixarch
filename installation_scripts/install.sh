aa924270cdcf874eea91335b93de86afc65be2d3
zote <zote@duck.com>
2023-01-18 11:43:14 -0500

    fixing grub now

--- installation_scripts/install.sh
+++ installation_scripts/install.sh
@@ -60,7 +60,7 @@ then
 	        sudo cp -r $LINKDOT/boot/grub/grubel /boot/grub/
 		sudo mkdir -p /etc/sddm.conf.d/
 		sudo cp -r $LINKDOT/boot/sddm/themes/pixarch_sddm /usr/share/sddm/themes/
-		sudo sed 's/\#GRUB_THEME/GRUB_THEME=\"\/boot\/grub\/grubel\/theme.txt\"/' -i /etc/default/grub
+		sudo sed 's/\#GRUB_THEME=\"path\/to\/gfxtheme\"/GRUB_THEME=\"\/boot\/grub\/grubel\/theme.txt\"/' -i /etc/default/grub
                 sudo cp $LINKDOT/installation_scripts/theme.conf /etc/sddm.conf.d/
 else 
 	echo "-- you're on your own for theming."
